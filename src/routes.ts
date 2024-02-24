f942c219d8e2ac4731abdf0d6683e44d9ce1a707
Thiago <thiago2b9s@gmail.com>
2024-02-24 02:27:15 -0300

    feat: send order feature

--- src/routes.ts
+++ src/routes.ts
@@ -19,6 +19,7 @@ import { CreateOrderController } from './controllers/order/CreateOrderController
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
+import { SendOrderController } from './controllers/order/SendOrderController';
 
 // MIDDLEWARE
 import { isAuthenticated } from './middlewares/isAuthenticated';
@@ -47,5 +48,6 @@ router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 
 export { router }
